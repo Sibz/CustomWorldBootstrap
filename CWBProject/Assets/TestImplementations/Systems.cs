8794b3d5e4fb30b3fb4c1760cdf0c1894622b4ee
Steven Turner <steven@entityzero.com>
2019-04-14 00:22:13 +1000

    Started Complete re-write

---
+++ CWBProject/Assets/TestImplementations/Systems.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+[DisableAutoCreation]
+public class Test1 : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
+[DisableAutoCreation]
+[CreateInWorld("Test2 World")]
+public class Test2 : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
+
+[DisableAutoCreation]
+[UpdateInGroup(typeof(InitializationSystemGroup))]
+[CreateInWorld("Test3 World")]
+public class Test3_InitializationSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
+[DisableAutoCreation]
+[UpdateInGroup(typeof(SimulationSystemGroup))]
+[CreateInWorld("Test3 World")]
+public class Test3_SimulationSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
+[DisableAutoCreation]
+[UpdateInGroup(typeof(PresentationSystemGroup))]
+[CreateInWorld("Test3 World")]
+public class Test3_PresentationSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
+[DisableAutoCreation]
+[CreateInWorld("Test4 World")]
+public class Test4_Group : ComponentSystemGroup
+{
+    protected override void OnUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
+[DisableAutoCreation]
+[UpdateInGroup(typeof(Test4_Group))]
+[CreateInWorld("Test4 World")]
+public class Test4_System : ComponentSystemGroup
+{
+    protected override void OnUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
+
+[DisableAutoCreation]
+[CreateInWorld("Test5 World")]
+public class Test5_Group1 : ComponentSystemGroup
+{
+    protected override void OnUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
+[DisableAutoCreation]
+[UpdateInGroup(typeof(Test5_Group1))]
+[CreateInWorld("Test5 World")]
+public class Test5_Group2 : ComponentSystemGroup
+{
+    protected override void OnUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
+[DisableAutoCreation]
+[UpdateInGroup(typeof(Test5_Group2))]
+[CreateInWorld("Test5 World")]
+public class Test5_System : ComponentSystemGroup
+{
+    protected override void OnUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
+
+[DisableAutoCreation]
+[CreateInWorld("Test6 World")]
+[UpdateAfter(typeof(Test6_System2))]
+public class Test6_System1 : ComponentSystemGroup
+{
+    protected override void OnUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
+[DisableAutoCreation]
+[CreateInWorld("Test6 World")]
+public class Test6_System2 : ComponentSystemGroup
+{
+    protected override void OnUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
+[DisableAutoCreation]
+[CreateInWorld("Test6 World")]
+[UpdateBefore(typeof(Test6_System2))]
+public class Test6_System3 : ComponentSystemGroup
+{
+    protected override void OnUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
