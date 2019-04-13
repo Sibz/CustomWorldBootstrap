60f5189c6a527dde159881a55e72705ee64cfb3f
Steven <steven@entityzero.com>
2019-04-14 04:35:26 +1000

    Create UpdateInGroupTests.cs
    Added tests to check systems are updated in their respective default groups (Initialization/Simulation/Presentation)

---
+++ Assets/Tests/UpdateInGroupTests.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.Entities;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+
+    public class UpdateInGroupTests : MyTestFixture
+    {
+        private const string WORLDNAME = "Test3 World";
+
+        [OneTimeSetUp]
+        protected override void OneTimeSetup()
+        {
+            base.OneTimeSetup();
+            m_DefaultSystems.Add(typeof(Test3_InitializationSystem));
+            m_DefaultSystems.Add(typeof(Test3_SimulationSystem));
+            m_DefaultSystems.Add(typeof(Test3_PresentationSystem));
+            var newSystems = new Initialiser(m_FakeCWB).Initialise(m_DefaultSystems);
+        }
+
+        [TearDown]
+        protected override void TearDown()
+        {
+
+        }
+
+        [Test]
+        public void Creates_Custom_World()
+        {
+            Assert.IsTrue(World.AllWorlds.Any(x => x.Name.Equals(WORLDNAME)));
+        }
+
+        [Test]
+        public void Systems_Are_In_Custom_World()
+        {
+            Assert.IsTrue(World.AllWorlds.Where(x => x.Name.Equals(WORLDNAME)).First().Systems.Any(x => x.GetType() == typeof(Test3_InitializationSystem)));
+            Assert.IsTrue(World.AllWorlds.Where(x => x.Name.Equals(WORLDNAME)).First().Systems.Any(x => x.GetType() == typeof(Test3_SimulationSystem)));
+            Assert.IsTrue(World.AllWorlds.Where(x => x.Name.Equals(WORLDNAME)).First().Systems.Any(x => x.GetType() == typeof(Test3_PresentationSystem)));
+        }
+
+        [Test]
+        public void Systems_Updates_In_UpdateGroup()
+        {
+            var world = World.AllWorlds.Where(x => x.Name.Equals(WORLDNAME)).First();
+            world.GetExistingSystem<InitializationSystemGroup>().Update();
+            Assert.IsTrue(world.GetExistingSystem<Test3_InitializationSystem>().Updated);
+            Assert.IsFalse(world.GetExistingSystem<Test3_SimulationSystem>().Updated);
+            Assert.IsFalse(world.GetExistingSystem<Test3_PresentationSystem>().Updated);
+            world.GetExistingSystem<SimulationSystemGroup>().Update();
+            Assert.IsTrue(world.GetExistingSystem<Test3_InitializationSystem>().Updated);
+            Assert.IsTrue(world.GetExistingSystem<Test3_SimulationSystem>().Updated);
+            Assert.IsFalse(world.GetExistingSystem<Test3_PresentationSystem>().Updated);
+            world.GetExistingSystem<PresentationSystemGroup>().Update();
+            Assert.IsTrue(world.GetExistingSystem<Test3_InitializationSystem>().Updated);
+            Assert.IsTrue(world.GetExistingSystem<Test3_SimulationSystem>().Updated);
+            Assert.IsTrue(world.GetExistingSystem<Test3_PresentationSystem>().Updated);
+        }
+    }
+}
