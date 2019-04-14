e258b50f591f9255ea5b73f8701efc2bd3bc0d94
Steven Turner <steven@entityzero.com>
2019-04-14 15:53:31 +1000

    Update UpdateInGroupTests.cs
    Split up some asserts into own tests

--- Assets/Tests/UpdateInGroupTests.cs
+++ Assets/Tests/UpdateInGroupTests.cs
@@ -31,28 +31,77 @@ namespace Tests
         }
 
         [Test]
-        public void Systems_Are_In_Custom_World()
+        public void TestInitializationSystem_Is_In_Custom_World()
         {
             Assert.IsTrue(SystemExistsInWorld(WORLDNAME, typeof(Test3_InitializationSystem)));
+        }
+
+        [Test]
+        public void TestSimulationSystem_Is_In_Custom_World()
+        {
             Assert.IsTrue(SystemExistsInWorld(WORLDNAME, typeof(Test3_SimulationSystem)));
+        }
+
+        [Test]
+        public void TestPresentationSystem_Is_In_Custom_World()
+        {
             Assert.IsTrue(SystemExistsInWorld(WORLDNAME, typeof(Test3_PresentationSystem)));
         }
 
         [Test]
-        public void Systems_Updates_In_UpdateGroup()
+        public void TestInitializationSystem_Updates_In_InitializationSystemGroup()
         {
             var world = GetWorld(WORLDNAME);
+            // Ensure updated values are false;
+            var initSys = world.GetExistingSystem<Test3_InitializationSystem>();
+            var simSys = world.GetExistingSystem<Test3_SimulationSystem>();
+            var presSys = world.GetExistingSystem<Test3_PresentationSystem>();
+            initSys.Updated = false;
+            simSys.Updated = false;
+            presSys.Updated = false;
+
             world.GetExistingSystem<InitializationSystemGroup>().Update();
+
             Assert.IsTrue(world.GetExistingSystem<Test3_InitializationSystem>().Updated);
             Assert.IsFalse(world.GetExistingSystem<Test3_SimulationSystem>().Updated);
             Assert.IsFalse(world.GetExistingSystem<Test3_PresentationSystem>().Updated);
+        }
+
+        [Test]
+        public void TestSimulationSystem_Updates_In_SimulationSystemGroup()
+        {
+            var world = GetWorld(WORLDNAME);
+            // Ensure updated values are false;
+            var initSys = world.GetExistingSystem<Test3_InitializationSystem>();
+            var simSys = world.GetExistingSystem<Test3_SimulationSystem>();
+            var presSys = world.GetExistingSystem<Test3_PresentationSystem>();
+            initSys.Updated = false;
+            simSys.Updated = false;
+            presSys.Updated = false;
+
             world.GetExistingSystem<SimulationSystemGroup>().Update();
-            Assert.IsTrue(world.GetExistingSystem<Test3_InitializationSystem>().Updated);
+
+            Assert.IsFalse(world.GetExistingSystem<Test3_InitializationSystem>().Updated);
             Assert.IsTrue(world.GetExistingSystem<Test3_SimulationSystem>().Updated);
             Assert.IsFalse(world.GetExistingSystem<Test3_PresentationSystem>().Updated);
+        }
+
+        [Test]
+        public void TestPresentationSystem_Updates_In_PresentationSystemGroup()
+        {
+            var world = GetWorld(WORLDNAME);
+            // Ensure updated values are false;
+            var initSys = world.GetExistingSystem<Test3_InitializationSystem>();
+            var simSys = world.GetExistingSystem<Test3_SimulationSystem>();
+            var presSys = world.GetExistingSystem<Test3_PresentationSystem>();
+            initSys.Updated = false;
+            simSys.Updated = false;
+            presSys.Updated = false;
+
             world.GetExistingSystem<PresentationSystemGroup>().Update();
-            Assert.IsTrue(world.GetExistingSystem<Test3_InitializationSystem>().Updated);
-            Assert.IsTrue(world.GetExistingSystem<Test3_SimulationSystem>().Updated);
+
+            Assert.IsFalse(world.GetExistingSystem<Test3_InitializationSystem>().Updated);
+            Assert.IsFalse(world.GetExistingSystem<Test3_SimulationSystem>().Updated);
             Assert.IsTrue(world.GetExistingSystem<Test3_PresentationSystem>().Updated);
         }
     }
