85191d791c9bc09945fc6705b21fd1953a9b68db
Steven Turner <steven@entityzero.com>
2019-04-14 16:02:24 +1000

    Added tests for PostInitialize

---
+++ Assets/Tests/PostInitializeTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.Entities;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class PostInitializeTests : MyTestFixture
+    {
+        protected override void Setup()
+        {
+            m_FakeCWB = new FakeCustomWorldBootstrap();
+        }
+
+        [Test]
+        public void PostInitialize_Runs()
+        {
+            new Initialiser(m_FakeCWB).Initialise(m_DefaultSystems);
+            Assert.IsTrue(m_FakeCWB.PostInitializeRan);
+        }
+
+        [Test]
+        public void Alterations_In_PostInitialize_Are_Returned()
+        {
+            m_FakeCWB.Alterations.Add(typeof(Test1));
+            var results = new Initialiser(m_FakeCWB).Initialise(m_DefaultSystems);
+            Assert.IsTrue(results.Contains(typeof(Test1)));
+        }
+    }
+}
--- Assets/Tests/Resources/FakeCustomWorldBootstrap.cs
+++ Assets/Tests/Resources/FakeCustomWorldBootstrap.cs
@@ -5,8 +5,12 @@ namespace Tests
 {
     public class FakeCustomWorldBootstrap : ICustomWorldBootstrap
     {
+        public List<Type> Alterations = new List<Type>();
+        public bool PostInitializeRan = false;
         public List<Type> PostInitialize(List<Type> systems)
         {
+            PostInitializeRan = true;
+            systems.AddRange(Alterations);
             return systems;
         }
     }
